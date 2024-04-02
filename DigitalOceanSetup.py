3c2d0a9d3ea520f5c875694d7361013c400fab99
Roman <begemaev01@gmail.com>
2024-04-02 13:24:30 +0700

    env_new_check

--- DigitalOceanSetup.py
+++ DigitalOceanSetup.py
@@ -3,18 +3,24 @@ import subprocess
 
 # Вводим имя домена
 domain_name = input("Enter domain name: ")
+print(f"Don't enter anything if you want to leave .env")
+print(f"ENV files are entered via _")
+env_name = input("enter_env:")
 
 # Список команд для выполнения
 commands = [
     f"rm /etc/nginx/sites-enabled/virusfreeprotect.com",
     f"cp /etc/nginx/sites-available/virusfreeprotect.com  /etc/nginx/sites-available/{domain_name}",
     f'sed -i "s/virusfreeprotect.com/{domain_name}/g" /etc/nginx/sites-available/{domain_name}',
+    f'sed -i "s/.env_a2/.env{env_name}/g" /etc/nginx/sites-available/{domain_name}',
     f"ln -s /etc/nginx/sites-available/{domain_name} /etc/nginx/sites-enabled/"
 ]
 
+
+
 # Выполнение командп
 for command in commands:
-    print(f"Выполняется команда: {command}")
+    print(f"Load command: {command}")
     result = subprocess.run(command, shell=True, capture_output=True, text=True)
     
     if result.returncode == 0:
